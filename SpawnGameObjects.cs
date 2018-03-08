a2aa8abca95e7990ee69d60626864b0f053667ba
Ladydiana <diana.culincu@gmail.com>
2018-03-08 18:04:03 +0200

    Prefab spawner.

---
+++ SpawnGameObjects.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnGameObjects : MonoBehaviour {
+
+	public GameObject spawnPrefab;
+
+	public float minSecondsBetweenSpawning = 3.0f;
+	public float maxSecondsBetweenSpawning = 6.0f;
+	
+	//public Transform chaseTarget;
+	
+	private float savedTime;
+	private float secondsBetweenSpawning;
+
+	// Use this for initialization
+	void Start () {
+		savedTime = Time.time;
+		secondsBetweenSpawning = Random.Range (minSecondsBetweenSpawning, maxSecondsBetweenSpawning);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time - savedTime >= secondsBetweenSpawning) // is it time to spawn again?
+		{
+			MakeThingToSpawn();
+			savedTime = Time.time; // store for next spawn
+			secondsBetweenSpawning = Random.Range (minSecondsBetweenSpawning, maxSecondsBetweenSpawning);
+		}	
+	}
+
+	void MakeThingToSpawn()
+	{
+		// create spawn position
+		Vector3 spawnPos = new Vector3(Random.Range(transform.position.x-100, transform.position.x +100), 
+					transform.position.y, 
+					Random.Range(transform.position.z-100, transform.position.z +100)
+					);
+		// create a new gameObject
+		GameObject clone = Instantiate(spawnPrefab, spawnPos, transform.rotation) as GameObject;
+		//GameObject clone = Instantiate(spawnPrefab, transform.position, transform.rotation) as GameObject;
+
+		// set chaseTarget if specified
+		/*if ((chaseTarget != null) && (clone.gameObject.GetComponent<Chaser> () != null))
+		{
+			clone.gameObject.GetComponent<Chaser>().SetTarget(chaseTarget);
+		}*/
+	}
+}
