8d7e6760ec1540613290fc03c0e0413b5fb967f5
Ladydiana <diana.culincu@gmail.com>
2018-03-13 16:53:03 +0200

    Teleport an object to a given position.

---
+++ TeleportToPosition.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * Teleports a game object to a given position.
+ */
+
+public class TeleportToPosition : MonoBehaviour
+{
+	public GameObject objectToTeleport;
+	public GameObject newLocation;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (objectToTeleport != null && newLocation!=null) {
+			objectToTeleport.transform.position = newLocation.transform.position;
+		}
+	}
+}
+
