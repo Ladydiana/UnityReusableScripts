6bf813ede3ba5e0a9e10af1c10d2357ca78c30f9
Ladydiana <diana.culincu@gmail.com>
2018-03-15 13:49:19 +0200

    Follows a given target.

---
+++ ChaserScript.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using System.Collections;
+
+
+/*
+ * Follows a given target.
+ */
+
+ 
+public class ChaserScript : MonoBehaviour {
+	
+	public float chaseSpeed = 20.0f;
+	public float minDistanceToTarget = 1.0f;
+	public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+		// if no target specified, assume the player
+		if (target == null) {
+			if (GameObject.FindWithTag ("Player")!=null) {
+				target = GameObject.FindWithTag ("Player");
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (target == null)
+			return;
+
+		// face the target
+		transform.LookAt(target.transform);
+
+		//get the distance between the chaser and the target
+		float distance = Vector3.Distance(transform.position, target.transform.position);
+
+		//as long as the chaser is farther away than the minimum distance, move towards it at chase speed.
+		if(distance > minDist)	{
+			transform.position += transform.forward * chaseSpeed * Time.deltaTime;
+		}			
+	}
+
+	// Set the target of the chaser
+	public void SetTarget(GameObject newTarget) {
+		target = newTarget;
+	}
+
+}
